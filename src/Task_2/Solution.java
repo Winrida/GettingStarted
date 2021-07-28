a82b7895ffa50730b04b6fe4a4ab84673962f0e7
Winrida <winrida@yandex.ru>
2021-07-28 20:50:49 +0300

    Task_2

---
+++ src/Task_2/Solution.java
@@ -0,0 +1,13 @@
+package Task_2;
+
+public class Solution
+{
+    public static void main(String[] args) {
+        System.out.print(sqr(5));
+    }
+
+    public static int sqr(int number)
+    {
+        return number * number;
+    }
+}
