0b3d5eefa19cec72d75a8b1e3dfccc9228dc0a2f
Winrida <winrida@yandex.ru>
2021-07-28 20:48:01 +0300

    Task_1

---
+++ src/Task_1/Solution.java
@@ -0,0 +1,12 @@
+package Task_1;
+
+public class Solution
+{
+    public static int number = 25;
+
+    public static void main(String[] args)
+    {
+        number = number * number;
+        System.out.print(number);
+    }
+}
