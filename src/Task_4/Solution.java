570b466fea6fab69400d80fa0aed4b1e54bb2fd3
Winrida <winrida@yandex.ru>
2021-07-28 20:55:37 +0300

    Task_4

---
+++ src/Task_4/Solution.java
@@ -0,0 +1,16 @@
+package Task_4;
+
+public class Solution
+{
+    public static void main(String[] args) {
+        print("Java is easy to learn!");
+        print("Java is object-oriented!");
+        print("Java is platform-independent!");
+    }
+
+    public static void print(String s)
+    {
+        for(int i = 0; i < 4; i++)
+            System.out.println(s);
+    }
+}
