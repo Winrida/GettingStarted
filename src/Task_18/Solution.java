76aa7b89cefd456f65356006428ac823cafbf772
Winrida <winrida@yandex.ru>
2021-08-02 15:31:23 +0300

    Task_18

---
+++ src/Task_18/Solution.java
@@ -0,0 +1,26 @@
+package Task_18;
+
+public class Solution
+{
+    public static void main(String[] args) {
+        displayClosestToTen(8, 11);
+        displayClosestToTen(7, 14);
+    }
+
+    public static void displayClosestToTen(int a, int b) {
+        if (abs(10 - a) <= abs(10 - b))
+        {
+            System.out.println(a);
+        }
+        else System.out.println(b);
+
+    }
+
+    public static int abs(int a) {
+        if (a < 0) {
+            return -a;
+        } else {
+            return a;
+        }
+    }
+}
