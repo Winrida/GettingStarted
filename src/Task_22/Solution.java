6c004b39727aebb2ffeee3d59de3df2fe6317b97
Winrida <winrida@yandex.ru>
2021-08-04 16:36:20 +0300

    Task_22

---
+++ src/Task_22/Solution.java
@@ -0,0 +1,19 @@
+package Task_22;
+
+public class Solution
+{
+    public static void main(String[] args) {
+        int i = 1;
+        while (i <= 10)
+        {
+            int j = 1;
+            while (j <= 10)
+            {
+                System.out.print((i * j) + " ");
+                j++;
+            }
+            System.out.println();
+            i++;
+        }
+    }
+}
