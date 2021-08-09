d5620614af1c4011ad5e0247b0e61ce16e86ded7
Winrida <winrida@yandex.ru>
2021-08-09 23:40:21 +0300

    Task_33

---
+++ src/Task_33/Solution.java
@@ -0,0 +1,29 @@
+package Task_33;
+
+public class Solution
+{
+    public class StaticClassExample
+    {
+        private static int catCount = 0;
+
+        public static void main(String[] args) throws Exception
+        {
+            Cat vaska = new Cat("Bella");
+            Cat murka = new Cat("Tiger");
+
+            System.out.println("Cat count " + catCount);
+        }
+
+        public static class Cat
+        {
+            private String name;
+
+            public Cat(String name)
+            {
+                this.name = name;
+                StaticClassExample.catCount++;
+            }
+        }
+
+    }
+}
