2ce57a0d1c371fb718d868f6ed889fecbb9e0af5
Winrida <winrida@yandex.ru>
2021-08-29 16:20:41 +0300

    Task_39

---
+++ src/Task_39/README.md
@@ -0,0 +1,4 @@
+# Блокнот для новых идей
+1. В классе Solution создать public static класс Idea.
+2. В классе Idea создать метод public String getDescription(), который будет возвращать любую строку.
+3. В классе Solution создай статический метод public void printIdea(Idea idea), который будет выводить на экран описание идеи - это то, что возвращает метод getDescription().
---
+++ src/Task_39/Solution.java
@@ -0,0 +1,18 @@
+package Task_39;
+
+public class Solution
+{
+    public static void main(String[] args) {
+        printIdea(new Idea());
+    }
+
+    public static void printIdea(Idea idea) {
+        System.out.println(idea.getDescription());
+    }
+
+    public static class Idea {
+        public String getDescription() {
+            return "Bob";
+        }
+    }
+}
