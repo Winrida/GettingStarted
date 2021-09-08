d83b25642b4e25d5fec9651b026da01ea3d5b97e
Winrida <winrida@yandex.ru>
2021-09-08 18:35:36 +0300

    Task_45

---
+++ src/Task_45/Solution.java
@@ -0,0 +1,27 @@
+package Task_45;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Solution
+{
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        int[] num = new int[15];
+        int a = 0, b = 0;
+        for (int i = 0; i < num.length; i++) {
+            num[i] = Integer.parseInt(reader.readLine());
+            if (i % 2 == 0)
+                b += num[i];
+            else
+                a += num[i];
+        }
+        if (a > b)
+            System.out.println("В домах с нечетными номерами проживает больше жителей.");
+        else if (a < b)
+            System.out.println("В домах с четными номерами проживает больше жителей.");
+        else
+            System.out.println("");
+    }
+}
