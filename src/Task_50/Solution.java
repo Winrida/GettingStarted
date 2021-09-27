977ef30fb95fd1262ba54c8edd29f0d43d2066ff
Winrida <winrida@yandex.ru>
2021-09-27 12:56:50 +0300

    Task_50

---
+++ src/Task_50/Solution.java
@@ -0,0 +1,34 @@
+package Task_50;
+
+import java.util.ArrayList;
+
+public class Solution
+{
+    public static void main(String[] args) {
+        ArrayList<String> strings = new ArrayList<String>();
+        strings.add("роза");
+        strings.add("лоза");
+        strings.add("лира");
+        strings = fix(strings);
+
+        for (String string : strings) {
+            System.out.println(string);
+        }
+    }
+
+    public static ArrayList<String> fix(ArrayList<String> strings) {
+
+        for (int i = 0; i < strings.size(); i++) {
+            if (strings.get(i).contains("л") == false && strings.get(i).contains("р") == true) {
+                strings.remove(i);
+                i--;
+
+            }else if (strings.get(i).contains("л") == true && strings.get(i).contains("р") == false) {
+                strings.add(i+1, strings.get(i));
+                i++;
+            }
+        }
+
+        return strings;
+    }
+}
