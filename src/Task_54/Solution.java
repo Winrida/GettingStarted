1049881842455dbef92a6da817507eb200726a2b
Winrida <winrida@yandex.ru>
2021-10-16 19:06:13 +0300

    Task_54

---
+++ src/Task_54/Solution.java
@@ -0,0 +1,12 @@
+package Task_54;
+
+import java.util.Date;
+
+public class Solution
+{
+    public static void main(String[] args) throws Exception
+    {
+        Date today = new Date();
+        System.out.println("Current date: " + today);
+    }
+}
