405eb957581cb82775f0833b0289df9e8731720c
Winrida <winrida@yandex.ru>
2021-10-18 18:51:38 +0300

    Task_57

---
+++ src/Task_57/Solution.java
@@ -0,0 +1,34 @@
+package Task_57;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
+public class Solution {
+    public static List getListForGet() {
+        ArrayList al = new ArrayList();
+        return al;
+    }
+
+    public static List getListForSet() {
+        ArrayList al = new ArrayList();
+        return al;
+
+    }
+
+    public static List getListForAddOrInsert() {
+        LinkedList ll = new LinkedList();
+        return ll;
+
+    }
+
+    public static List getListForRemove() {
+        LinkedList ll = new LinkedList();
+        return ll;
+
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
