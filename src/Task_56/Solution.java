341b52eabcba966cf6aa6bfd0dcccb3a1b8a36c5
Winrida <winrida@yandex.ru>
2021-10-18 18:41:18 +0300

    Task_56

---
+++ src/Task_56/Solution.java
@@ -0,0 +1,29 @@
+package Task_56;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.LinkedList;
+import java.util.List;
+
+public class Solution {
+    public static void main(String[] args) {
+        System.out.println(getInsertTimeInMs(new ArrayList()));
+        System.out.println(getInsertTimeInMs(new LinkedList()));
+    }
+
+    public static long getInsertTimeInMs(List list) {
+        Date currentTime = new Date();
+
+        insert10000(list);
+
+        Date newTime = new Date();
+        return newTime.getTime() - currentTime.getTime();
+
+    }
+
+    public static void insert10000(List list) {
+        for (int i = 0; i < 10000; i++) {
+            list.add(0, new Object());
+        }
+    }
+}
