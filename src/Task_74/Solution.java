5738a43a4aaef50b4fb346718e9e34943b77935d
Winrida <winrida@yandex.ru>
2021-11-08 21:26:29 +0300

    Task_74

---
+++ src/Task_74/Solution.java
@@ -0,0 +1,39 @@
+package Task_74;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
+public class Solution {
+    public static void main(String[] args) throws Exception {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+        ArrayList<String> words = new ArrayList<String>();
+        for (int i = 0; i < 20; i++) {
+            words.add(reader.readLine());
+        }
+
+        Map<String, Integer> map = countWords(words);
+
+        for (Map.Entry<String, Integer> pair : map.entrySet()) {
+            System.out.println(pair.getKey() + " " + pair.getValue());
+        }
+    }
+
+    public static Map<String, Integer> countWords(ArrayList<String> list) {
+        HashMap<String, Integer> result = new HashMap<String, Integer>();
+
+        for (String element : list) {
+            if (result.containsKey(element)) {
+                result.put(element, result.get(element)+1);
+            }
+            else result.put(element, 1);
+        }
+
+
+        return result;
+    }
+
+}
